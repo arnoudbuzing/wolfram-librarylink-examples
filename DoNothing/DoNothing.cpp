cd4dd7545e9f042759db8fdc97157a3025decf65
Arnoud Buzing <arnoudb@wolfram.com>
2018-06-27 16:40:11 -0500

    updates

--- DoNothing/DoNothing.cpp
+++ DoNothing/DoNothing.cpp
@@ -1,5 +1,9 @@
 #include "WolframLibrary.h"
 
+EXTERN_C DLLEXPORT mint WolframLibrary_getVersion( ) { return WolframLibraryVersion; }
+EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData) { return 0; }
+EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData) { return; }
+
 EXTERN_C DLLEXPORT int DoNothing(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
         return LIBRARY_NO_ERROR;
 }
