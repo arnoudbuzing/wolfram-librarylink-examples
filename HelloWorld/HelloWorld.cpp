ac8eff70237472a9922b6266e08916c55248e630
Arnoud Buzing <arnoudb@wolfram.com>
2018-06-27 16:38:31 -0500

    updates

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,10 +1,8 @@
 #include "WolframLibrary.h"
 
-/*
 EXTERN_C DLLEXPORT mint WolframLibrary_getVersion( ) { return WolframLibraryVersion; }
 EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData) { return 0; }
 EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData) { return; }
-*/
 
 EXTERN_C DLLEXPORT int HelloWorld(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
 	const char* hello = "Hello World";
