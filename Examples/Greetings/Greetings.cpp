9d862f2ab3572510db5ae2015ccc89f1025eef38
Arnoud Buzing <arnoudb@wolfram.com>
2018-06-28 13:38:17 -0500

    updates

--- Examples/Greetings/Greetings.cpp
+++ Examples/Greetings/Greetings.cpp
@@ -5,11 +5,11 @@ EXTERN_C DLLEXPORT mint WolframLibrary_getVersion( ) { return WolframLibraryVers
 EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData) { return 0; }
 EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData) { return; }
 
-EXTERN_C  DLLEXPORT int Greetings(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
+EXTERN_C DLLEXPORT int Greetings(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
   char *name;
   char result[1024];
   name = MArgument_getUTF8String(Args[0]);
-  sprintf_s(result,"Greetings %s!",name);
+  sprintf_s(result,1024,"Greetings %s!", name);
   MArgument_setUTF8String(Res,result);
   return LIBRARY_NO_ERROR;
 }
