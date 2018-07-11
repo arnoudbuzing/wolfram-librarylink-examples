0c2ee41ff101162a4e010ad9743033ec72fafe37
Arnoud Buzing <arnoudb@wolfram.com>
2018-07-11 15:33:24 -0500

    updates

--- 02-IntermediateExamples/Asynchronous/AsynchronousReals/AsynchronousReals.cpp
+++ 02-IntermediateExamples/Asynchronous/AsynchronousReals/AsynchronousReals.cpp
@@ -1,5 +1,5 @@
-#include <stdlib.h>
-#include <windows.h>
+#include <stdlib.h> // for malloc function
+#include <windows.h> // For Sleep function
 
 #include "WolframLibrary.h"
 #include "WolframIOLibraryFunctions.h"
@@ -8,49 +8,62 @@ EXTERN_C DLLEXPORT mint WolframLibrary_getVersion( ) { return WolframLibraryVers
 EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData) { return 0; }
 EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData) { return; }
 
-typedef struct thread_struct
+typedef struct background_struct
 {
-	WolframIOLibrary_Functions ioLibrary;
-	mint pausemillis;
-	mreal eventdata;
-} *thread_struct_pointer;
+	WolframLibraryData libData;
+	mint pause_milliseconds;
+	mreal background_real_input;
+} *background_struct_pointer;
 
 static void return_real(mint, void*);
 static void run_background_task(mint, void*);
 
+//
+// Wolfram Language level functions
+//
+
 EXTERN_C DLLEXPORT int start_background_task(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res)
+{
+	background_struct_pointer background_data = (background_struct_pointer)malloc(sizeof(struct background_struct));
+	background_data->libData = libData;
+	background_data->pause_milliseconds = MArgument_getInteger(Args[0]);
+	background_data->background_real_input = MArgument_getReal(Args[1]);
+
+	mint asyncid;
+	asyncid = libData->ioLibraryFunctions->createAsynchronousTaskWithThread(run_background_task, background_data);
+	MArgument_setInteger(Res, asyncid);
+
+	return LIBRARY_NO_ERROR;
+}
+
+EXTERN_C DLLEXPORT int stop_background_task(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res)
 {
 	mint asyncid;
 	WolframIOLibrary_Functions ioLibrary = libData->ioLibraryFunctions;
-	thread_struct_pointer thread_arguments = (thread_struct_pointer)malloc(sizeof(struct thread_struct));
-	if(Argc != 2)
-		return LIBRARY_FUNCTION_ERROR;
-	thread_arguments->ioLibrary = ioLibrary;
-	thread_arguments->pausemillis = MArgument_getInteger(Args[0]);
-	thread_arguments->eventdata = MArgument_getReal(Args[1]);
-	asyncid = ioLibrary->createAsynchronousTaskWithThread(run_background_task, thread_arguments);
-	MArgument_setInteger(Res, asyncid);
+	asyncid = MArgument_getInteger(Args[0]);
+	ioLibrary->removeAsynchronousTask(asyncid);
 	return LIBRARY_NO_ERROR;
+}
+
+//
+// Library private functions
+//
+
+static void run_background_task(mint asyncid, void* vtarg) {
+	background_struct_pointer targ = (background_struct_pointer) vtarg;
+	while(1) { // run forever until stopped or aborted
+		mint abortedQ = targ->libData->AbortQ();
+		if(abortedQ) break;
+		Sleep( (int) targ->pause_milliseconds );
+		return_real(asyncid, vtarg);
+	}
 }
 
 static void return_real(mint asyncid, void* vtarg)
 {
-	thread_struct_pointer targ = (thread_struct_pointer)vtarg;
-	WolframIOLibrary_Functions ioLibrary = targ->ioLibrary;
-	mint pausems = targ->pausemillis;
-	mreal eventdata = targ->eventdata;
+	background_struct_pointer targ = (background_struct_pointer) vtarg;
 	DataStore ds;
-	free(targ);
-
-	Sleep((int)pausems);
-	ds = ioLibrary->createDataStore();
-	ioLibrary->DataStore_addReal(ds, eventdata);
-	ioLibrary->raiseAsyncEvent(asyncid, "return_real", ds);
-}
-
-static void run_background_task(mint asyncid, void* vtarg)
-{
-		while(1) {
-				return_real(asyncid,vtarg);
-		}
+	ds = targ->libData->ioLibraryFunctions->createDataStore();
+	targ->libData->ioLibraryFunctions->DataStore_addReal(ds, targ->background_real_input );
+	targ->libData->ioLibraryFunctions->raiseAsyncEvent(asyncid, "return_real", ds);
 }
