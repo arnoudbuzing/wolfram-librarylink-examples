394782e08017a4de96fa8a1bd04f74f4c41daa01
Arnoud Buzing <arnoudb@wolfram.com>
2018-07-11 13:56:15 -0500

    updates

--- 02-IntermediateExamples/Asynchronous/AsynchronousReals/AsynchronousReals.cpp
+++ 02-IntermediateExamples/Asynchronous/AsynchronousReals/AsynchronousReals.cpp
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <windows.h>
+
 #include "WolframLibrary.h"
 #include "WolframIOLibraryFunctions.h"
 
@@ -5,28 +8,17 @@ EXTERN_C DLLEXPORT mint WolframLibrary_getVersion( ) { return WolframLibraryVers
 EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData) { return 0; }
 EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData) { return; }
 
-/*
-EXTERN_C DLLEXPORT int Addition(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
-	mint x,y;
-	mint result;
-	x = MArgument_getInteger(Args[0]);
-	y = MArgument_getInteger(Args[1]);
-	result = x+y;
-	MArgument_setInteger(Res,result);
-	return LIBRARY_NO_ERROR;
-}
-*/
-
 typedef struct thread_struct
 {
 	WolframIOLibrary_Functions ioLibrary;
-	int pausemillis;
+	mint pausemillis;
 	mreal eventdata;
 } *thread_struct_pointer;
 
+static void return_real(mint, void*);
 static void run_background_task(mint, void*);
 
-DLLEXPORT int start_background_task(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res)
+EXTERN_C DLLEXPORT int start_background_task(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res)
 {
 	mint asyncid;
 	WolframIOLibrary_Functions ioLibrary = libData->ioLibraryFunctions;
@@ -41,7 +33,7 @@ DLLEXPORT int start_background_task(WolframLibraryData libData, mint Argc, MArgu
 	return LIBRARY_NO_ERROR;
 }
 
-static void run_background_task(mint asyncid, void* vtarg)
+static void return_real(mint asyncid, void* vtarg)
 {
 	thread_struct_pointer targ = (thread_struct_pointer)vtarg;
 	WolframIOLibrary_Functions ioLibrary = targ->ioLibrary;
@@ -50,8 +42,15 @@ static void run_background_task(mint asyncid, void* vtarg)
 	DataStore ds;
 	free(targ);
 
-	Sleep(pausems);
+	Sleep((int)pausems);
 	ds = ioLibrary->createDataStore();
 	ioLibrary->DataStore_addReal(ds, eventdata);
-	ioLibrary->raiseAsyncEvent(asyncid, NULL, ds);
+	ioLibrary->raiseAsyncEvent(asyncid, "return_real", ds);
+}
+
+static void run_background_task(mint asyncid, void* vtarg)
+{
+		while(1) {
+				return_real(asyncid,vtarg);
+		}
 }
