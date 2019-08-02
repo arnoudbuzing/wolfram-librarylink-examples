657a9aba83bdeee02f2d7486ff1657f742375b26
Arnoud Buzing <arnoudb@wolfram.com>
2019-08-02 16:44:07 -0500

    updates

---
+++ 01-BasicExamples/02-Strings/StringToNumber/README.md
@@ -0,0 +1,7 @@
+# StringToNumber
+
+This example shows how to use the standard C library `strtol` and `strtod` functions to parse strings into numbers and return them to the Wolfram Language.
+
+This will create two Wolfram Language functions, also called `strtol` and strtod`.
+
+
---
+++ 01-BasicExamples/02-Strings/StringToNumber/StringToNumber.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "WolframLibrary.h"
+
+EXTERN_C DLLEXPORT mint WolframLibrary_getVersion( ) { return WolframLibraryVersion; }
+EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData) { return 0; }
+EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData) { return; }
+
+EXTERN_C DLLEXPORT int wolfram_strtol(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
+  char *string;
+  mint base;
+  mint result;
+  string = MArgument_getUTF8String(Args[0]);
+  base = MArgument_getInteger(Args[1]);
+  result = (mint) strtol(string, NULL, base);
+  MArgument_setInteger(Res,result);
+  return LIBRARY_NO_ERROR;
+}
+
+EXTERN_C DLLEXPORT int wolfram_strtod(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
+  char *string;
+  mint base;
+  mreal result;
+  string = MArgument_getUTF8String(Args[0]);
+  base = MArgument_getInteger(Args[1]);
+  result = strtod(string, NULL, base);
+  MArgument_setReal(Res,result);
+  return LIBRARY_NO_ERROR;
+}
